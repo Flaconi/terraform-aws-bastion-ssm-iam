05d8c545d4e33bac43ccd71ea5df3bb1c4180230
Bernhardt Frank (GS/PJ-TOP98) <Frank.Bernhardt@de.bosch.com>
2022-02-23 10:06:16 +0100

    extra_tags and image_id input added

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "ssm_document_name" {
   description = "The document name of SSM"
   value       = local.ssm_document_name
 }
+
+output "instance_profile_name" {
+  description = "The instance profile name of SSM"
+  value       = aws_iam_instance_profile.this.name
+}
